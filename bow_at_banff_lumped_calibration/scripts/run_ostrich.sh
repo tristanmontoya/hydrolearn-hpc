15efaad771995956660eeebd27d850f1380a19fb
Tristan Montoya <montoya.tristan@gmail.com>
2026-06-25 21:08:10 -0400

    cleanup/comments

--- bow_at_banff_lumped_calibration/scripts/run_ostrich.sh
+++ bow_at_banff_lumped_calibration/scripts/run_ostrich.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=bow_lumped_pdds
+#SBATCH --job-name=bow_at_banff_parallel_calibration
 #SBATCH --nodes=2
-#SBATCH --ntasks=4
 #SBATCH --ntasks-per-node=2
-#SBATCH --mem-per-cpu=400M
 #SBATCH --time=02:00:00
 #SBATCH --output=slurm-%x-%j.out
 set -euo pipefail
@@ -13,5 +11,5 @@ export PYTHON="${PYTHON:-python}"
 export SUMMA_EXE="${SUMMA_EXE:-summa.exe}"
 export PARALLEL_CALIBRATION_ROOT="${PWD}"
 
-# Start OSTRICH
-srun --kill-on-bad-exit=1 OstrichMPI
+# Replaced `ostrich` with `OstrichMPI` to enable parallel calibration
+srun OstrichMPI
